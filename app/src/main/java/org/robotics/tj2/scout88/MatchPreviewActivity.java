727156fa910dda697c96992c3d04d3a8719ff1cf
wallr1 <wallr1@wit.edu>
2019-02-20 16:44:07 -0500

    adding match preview screen

--- app/src/main/java/org/robotics/tj2/scout88/MatchPreviewActivity.java
+++ app/src/main/java/org/robotics/tj2/scout88/MatchPreviewActivity.java
@@ -1,19 +1,25 @@
 package org.robotics.tj2.scout88;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MatchPreviewActivity extends AppCompatActivity {
+
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +37,58 @@ public class MatchPreviewActivity extends AppCompatActivity {
         }
 
         final FirebaseInterface fbi = new FirebaseInterface();
-        ArrayList<Performance> alpBlue = fbi.getMatchPreviewData(Arrays.copyOfRange(teamNums , 0 , 2));
-        ArrayList<Performance> alpRed = fbi.getMatchPreviewData(Arrays.copyOfRange(teamNums , 3 , 5));
+        ArrayList<Performance> alpBlueAll = fbi.getMatchPreviewData(Arrays.copyOfRange(teamNums , 0 , 2));
+        ArrayList<Performance> alpRedAll = fbi.getMatchPreviewData(Arrays.copyOfRange(teamNums , 3 , 5));
 
         TextView blue1Number = (TextView) findViewById(R.id.blue1);
         TextView blue2Number = (TextView) findViewById(R.id.blue2);
         TextView blue3Number = (TextView) findViewById(R.id.blue3);
         TextView red1Number = (TextView) findViewById(R.id.red1);
+        TextView red2Number = (TextView) findViewById(R.id.red2);
+        TextView red3Number = (TextView) findViewById(R.id.red3);
 
         blue1Number.setText(teamNums[0] + "");
         blue2Number.setText(teamNums[1] + "");
         blue3Number.setText(teamNums[2] + "");
         red1Number.setText(teamNums[3] + "");
+        red2Number.setText(teamNums[4] + "");
+        red3Number.setText(teamNums[5] + "");
+
+        final int winPercBlue = 67;
+        final int winPercRed = 100 - winPercBlue;
+
+        final ProgressBar winPercBar = (ProgressBar) findViewById(R.id.progressBar);
+        //winPercBar.getProgressDrawable().setColorFilter(0x303F9F , PorterDuff.Mode.SRC_IN);
+        // Start long running operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                int direction = 1;
+                int sleepTime = 50;
+                while (progressStatus != winPercBlue) {
+
+                    progressStatus += direction;
+                    // Update the progress bar and display the
+                    //current value in the text view
+
+                    if(progressStatus+10 == winPercBlue){
+                        sleepTime = 100;
+                    }
+                    handler.post(new Runnable() {
+                        public void run() {
+                            winPercBar.setProgress(progressStatus);
+
+                        }
+                    });
+                    try {
+
+                        Thread.sleep(sleepTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
 
     }
 }
