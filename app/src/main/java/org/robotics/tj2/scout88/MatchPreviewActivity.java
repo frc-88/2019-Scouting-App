646eb9298ae97657602e42399fef775dc0d8676e
wallr1 <wallr1@wit.edu>
2019-02-20 21:13:58 -0500

    hi

--- app/src/main/java/org/robotics/tj2/scout88/MatchPreviewActivity.java
+++ app/src/main/java/org/robotics/tj2/scout88/MatchPreviewActivity.java
@@ -1,9 +1,11 @@
 package org.robotics.tj2.scout88;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -18,7 +20,7 @@ import java.util.Arrays;
 
 public class MatchPreviewActivity extends AppCompatActivity {
 
-    private int progressStatus = 0;
+    private int progressStatus = 50;
     private Handler handler = new Handler();
 
     @Override
@@ -54,28 +56,40 @@ public class MatchPreviewActivity extends AppCompatActivity {
         red2Number.setText(teamNums[4] + "");
         red3Number.setText(teamNums[5] + "");
 
-        final int winPercBlue = 67;
+        final int winPercBlue = 88;
         final int winPercRed = 100 - winPercBlue;
 
         final ProgressBar winPercBar = (ProgressBar) findViewById(R.id.progressBar);
+        final TextView textBlueWinPerc = (TextView) findViewById(R.id.blue_win_perc_text);
+        final TextView textRedWinPerc = (TextView) findViewById(R.id.red_win_perc_text);
+
+
         //winPercBar.getProgressDrawable().setColorFilter(0x303F9F , PorterDuff.Mode.SRC_IN);
         // Start long running operation in a background thread
         new Thread(new Runnable() {
             public void run() {
-                int direction = 1;
-                int sleepTime = 50;
+                int direction = -1;
+                int sleepTime = 25;
+
+                if(winPercBlue >= 50){
+                    direction = 1;
+                }
+
                 while (progressStatus != winPercBlue) {
 
                     progressStatus += direction;
                     // Update the progress bar and display the
                     //current value in the text view
 
-                    if(progressStatus+10 == winPercBlue){
-                        sleepTime = 100;
+                    if(Math.abs(winPercBlue - progressStatus) == 10){
+                        sleepTime *= 2;
                     }
                     handler.post(new Runnable() {
                         public void run() {
                             winPercBar.setProgress(progressStatus);
+                            textBlueWinPerc.setText(progressStatus + "");
+                            int rwp = 100-progressStatus;
+                            textRedWinPerc.setText(rwp + "");
 
                         }
                     });
